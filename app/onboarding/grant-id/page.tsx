27c3854f16459d1fdf3144f61c2698e67c3b3653
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2025-02-17 22:02:46 +0530

    Connecting to Nylas

--- app/onboarding/grant-id/page.tsx
+++ app/onboarding/grant-id/page.tsx
@@ -28,7 +28,7 @@ export default function OnboardingrouteTwo() {
         </CardHeader>
         <CardContent>
           <Button asChild className="w-full">
-            <Link href="/">
+            <Link href="/api/auth">
               <CalendarCheck2 className="size-4 mr-2" />
               Connect Calendar to your Account
             </Link>
