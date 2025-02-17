757b36f39a0c97f12cb2b1f29b6f7a75f757e642
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2025-02-17 20:08:15 +0530

    onboarding route
    added the new onboarding route for connecting of calendar

--- app/actions.ts
+++ app/actions.ts
@@ -1,20 +1,42 @@
-"use server"
+"use server";
 
-import { prisma } from "./lib/db"
-import { requireUser } from "./lib/hooks"
+import { onboardingSchema, onboardingSchemaValidation } from "@/lib/zodSchemas";
+import { prisma } from "./lib/db";
+import { requireUser } from "./lib/hooks";
+import { parseWithZod } from "@conform-to/zod";
+import { redirect } from "next/navigation";
 
+export async function OnboardingAction(prevState: any, formData: FormData) {
+  const session = await requireUser();
 
-export async function OnboardingAction(formData: FormData) {
-    const session = await requireUser()
-    const data = await prisma.user.update({
-        where: {
-            id: session.user?.id,
-        },
-        data: {
-            userName: "adfasfd",
-            name: "adfasdd"
-        },
-    });
+  const submission = await parseWithZod(formData, {
+    schema: onboardingSchemaValidation({
+      async isUsernameUnique() {
+        const existingUsername = await prisma.user.findUnique({
+          where: {
+            userName: formData.get("userName") as string,
+          },
+        });
+        return !existingUsername;
+      },
+    }),
+    async: true,
+  });
 
-    formData.get('username')
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
+  const data = await prisma.user.update({
+    where: {
+      id: session.user?.id,
+    },
+    data: {
+      userName: submission.value.userName,
+      name: submission.value.fullName,
+    },
+  });
+
+  //   formData.get("username");
+  return redirect("/dashboard");
 }
