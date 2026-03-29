ea18cb2422afb0cc4adc7eb7c105c0f687d31c69
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2026-03-29 16:21:21 +0530

    feat: add Google verification to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,6 +55,9 @@ export const metadata = {
     canonical: "https://caleasy.vercel.app",
   },
   robots: "index, follow",
+  verification: {
+    google: "QgVm-W9_sWA8B47coaItOUCg-3pX84KbM2wEtP9Jpm4"
+  },
 };
 
 export default function RootLayout({
